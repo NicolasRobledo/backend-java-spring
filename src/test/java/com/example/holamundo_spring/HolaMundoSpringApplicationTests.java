e790c384661e837224473433905be61c59e60d22
Nicolas Robledo <tomatekiwi84@gmail.com>
2025-04-02 13:54:03 -0300

    primer commit

---
+++ src/test/java/com/example/holamundo_spring/HolaMundoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holamundo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
